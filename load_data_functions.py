7c27971317e071d552a3277f5336019232ce8a78
Lovisa Hagstroem <lovhagstrom@gmail.com>
2020-09-22 16:56:16 +0200

    Add better handling of json lines.

--- load_data_functions.py
+++ load_data_functions.py
@@ -46,20 +46,19 @@ def get_data_frame_from_file(filename, fields, **kwargs):
             parser = ijson.parse(input_file, multiple_values=True)
             current_nbr_items = 0
             for prefix, event, value in parser:
-                #print("%s: %s" %(prefix,value))
+                if prefix == '':
+                    if event == 'start_map':
+                        data_entry = {}
+                    elif event == 'end_map':
+                        if (not wanted_values or 
+                        (wanted_values and _data_entry_is_wanted(data_entry, wanted_values))):
+                            # add entry to data
+                            for key, value in data_entry.items():
+                                data_dict[key].append(value)
 
-                if event == 'start_map':
-                    data_entry = {}
-                elif event == 'end_map':
-                    if (not wanted_values or 
-                    (wanted_values and _data_entry_is_wanted(data_entry, wanted_values))):
-                        # add entry to data
-                        for key, value in data_entry.items():
-                            data_dict[key].append(value)
-
-                        current_nbr_items += 1
-                        if max_nbr_items <= current_nbr_items :
-                            break
+                            current_nbr_items += 1
+                            if max_nbr_items <= current_nbr_items :
+                                break
                 else:
                     if prefix in fields:
                         data_entry[prefix] = value
