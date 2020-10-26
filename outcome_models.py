0c0bbf89bb24c436da0aebe0389bbdbb52af9d2e
Lovisa Hagström <lovhagstrom@gmail.com>
2020-10-26 13:59:02 +0100

    Add dummy model.

--- outcome_models.py
+++ outcome_models.py
@@ -1,4 +1,8 @@
 from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.dummy import DummyClassifier
+
+def get_baseline_model():
+    return DummyClassifier(strategy="most_frequent")
 
 def get_linear_model():
     return LinearRegression()
