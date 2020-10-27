108f1d23e512a8cb7d64b9712ee12abe5dd63d0a
antmats <anton.mattsson94@gmail.com>
2020-10-27 20:40:58 +0100

    Fixed bug in _get_scaled_data().

--- result_help_functions.py
+++ result_help_functions.py
@@ -3,13 +3,28 @@ import itertools
 from sklearn import preprocessing
 
 class Data:
-    def __init__(self, data, treatments, confounders, include_stars=True):
-        self.data = data
+    def __init__(self, data, treatments, confounders_cont, confounders_cat):
+        self.data = data.copy()
+        
+        # Use dummy encoding
+        assert 'stars' in confounders_cat
+        assert 'is_positive_sentiment' in confounders_cat
+        assert len(confounders_cat) == 2
+        stars_dummies = pd.get_dummies(self.data['stars'], drop_first=True, prefix='stars')
+        self.data = pd.concat([self.data, stars_dummies], axis=1)
+        self.data.drop(columns='stars', inplace=True)
+        self.data['is_positive_sentiment'] = self.data['is_positive_sentiment']. astype(int)
+        
+        self.confounders_cat = stars_dummies.columns.tolist()
+        self.confounders_cat.append('is_positive_sentiment')
+
+        self.confounders_cont = confounders_cont
+        
         self.treatments = treatments
         self.n_treatments = len(treatments)
+        
         self.treatment_groups = list(itertools.product([0, 1], repeat=self.n_treatments))
-        self.confounders = confounders
-        self.include_stars = include_stars
+        
         self._get_data()
         self._get_scaled_data()
         
@@ -17,22 +32,20 @@ class Data:
         self.X_train_scaled = dict.fromkeys(self.treatment_groups)
         self.X_test_scaled = dict.fromkeys(self.treatment_groups)
         
-        confounders_scaled = preprocessing.scale(self.data[self.confounders])
-        confounders_scaled = pd.DataFrame(confounders_scaled, index=self.data.index, columns=self.confounders)
+        confounders_scaled = preprocessing.scale(self.data[self.confounders_cont])
+        confounders_scaled = pd.DataFrame(confounders_scaled, index=self.data.index, columns=self.confounders_cont)
         
         treatment_groups = self.data.groupby(self.treatments)
         
-        for tg, indexes in treatment_groups.groups.items():
-            filter_col = [col for col in self.X_train[tg] if col.startswith('stars')]
-            
-            self.X_train_scaled[tg] = self.X_train[tg][filter_col].copy()
-            self.X_test_scaled[tg] = self.X_test[tg][filter_col].copy()
+        for tg in self.treatment_groups:            
+            self.X_train_scaled[tg] = self.X_train[tg][self.confounders_cat].copy()
+            self.X_test_scaled[tg] = self.X_test[tg][self.confounders_cat].copy()
             
             indexes_train = self.X_train[tg].index
             indexes_test = self.X_test[tg].index
-        
-            self.X_train_scaled[tg][self.confounders] = confounders_scaled.iloc[indexes_train]
-            self.X_test_scaled[tg][self.confounders] = confounders_scaled.iloc[indexes_test]
+            
+            self.X_train_scaled[tg][self.confounders_cont] = confounders_scaled.iloc[indexes_train]
+            self.X_test_scaled[tg][self.confounders_cont] = confounders_scaled.iloc[indexes_test]
             
     def _get_data(self):
         self.X_train = dict.fromkeys(self.treatment_groups)
@@ -50,12 +63,11 @@ class Data:
             temp_data_train = self.data.iloc[indexes].loc[self.data['test'] == 0]
             temp_data_test = self.data.iloc[indexes].loc[self.data['test'] == 1]
             
-            if self.include_stars:
-                self.X_train[tg] = pd.get_dummies(temp_data_train['stars'], drop_first=True, prefix='stars')
-                self.X_test[tg] = pd.get_dummies(temp_data_test['stars'], drop_first=True, prefix='stars')
+            self.X_train[tg] = temp_data_train[self.confounders_cat].copy()
+            self.X_test[tg] = temp_data_test[self.confounders_cat].copy()
             
-            self.X_train[tg][self.confounders] = temp_data_train[self.confounders]
-            self.X_test[tg][self.confounders] = temp_data_test[self.confounders]
+            self.X_train[tg][self.confounders_cont] = temp_data_train[self.confounders_cont]
+            self.X_test[tg][self.confounders_cont] = temp_data_test[self.confounders_cont]
             
             self.Y_train[tg] = temp_data_train['useful']
             self.Y_test[tg] = temp_data_test['useful']
