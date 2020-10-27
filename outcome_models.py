e1f7b8724bff46cd6715f8bd3da563f9cb7a5848
Lovisa Hagström <lovhagstrom@gmail.com>
2020-10-27 13:47:51 +0100

    Add evaluation of models not splitted over treatments.

--- outcome_models.py
+++ outcome_models.py
@@ -16,3 +16,9 @@ def get_random_forest_model_1():
 
 def get_random_forest_model_2():
     return RandomForestClassifier(n_estimators=100, max_depth=None, min_samples_split=5)
+
+def get_random_forest_model_3():
+    return RandomForestClassifier(n_estimators=100, max_depth=None, min_samples_split=7)
+
+def get_random_forest_model_4():
+    return RandomForestClassifier(n_estimators=100, max_depth=None, min_samples_split=10)
